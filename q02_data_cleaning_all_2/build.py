b67e9217b80789787c772bfc1d6c6fc74518c311
mritunjaysingh07 <mj.singh07@gmail.com>
2017-11-05 08:19:36 +0000

    Done

--- q02_data_cleaning_all_2/build.py
+++ q02_data_cleaning_all_2/build.py
@@ -12,15 +12,23 @@ loan_data = outlier_removal(loan_data)
 X, y, X_train, X_test, y_train, y_test = data_cleaning(loan_data)
 
 
-def data_cleaning_2(X_train,X_test,y_train,y_test):
+def data_cleaning_2(X_train,X_test,y_train,y_test) :
 
-    df_trans['GrLivArea'] = np.sqrt(df_trans['GrLivArea'])
+    for i in X_train.select_dtypes(include=[np.number]).columns.tolist() :
+        np.sqrt(X_train[i])
+
+    for i in X_test.select_dtypes(include=[np.number]).columns.tolist() :
+        np.sqrt(X_test[i])
+
+    X_test = pd.get_dummies(X_test, drop_first = True )
+    #print(X_test.shape)
 
     X_train = pd.get_dummies(X_train, drop_first = True )
-    X_test =  pd.get_dummies( X_test, drop_first = True )
 
+    #X_train.drop(mylist, axis=1, inplace = True)
+    #print(X_train.shape)
+    #print(list(X_test['Dependents_1'].value_counts()))
 
-    return X_train,X_test
-data_cleaning_2(X_train,X_test,y_train,y_test)
+    return X_train, X_test, y_train, y_test
 
 # Write your solution here :
