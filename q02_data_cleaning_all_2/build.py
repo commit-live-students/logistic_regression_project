301c7809508bbf35e536deee3a23bf7bbca41543
Sakshi-BUK <sakshi.dhokrat96@gmail.com>
2017-11-13 07:29:15 +0000

    Done

--- q02_data_cleaning_all_2/build.py
+++ q02_data_cleaning_all_2/build.py
@@ -1,3 +1,4 @@
+# %load q02_data_cleaning_all_2/build.py
 # Default Imports
 
 import pandas as pd
@@ -13,4 +14,23 @@ X, y, X_train, X_test, y_train, y_test = data_cleaning(loan_data)
 
 
 # Write your solution here :
+def data_cleaning_2 (X_train, X_test, y_train, y_test):
+    numerics=['int16','int32','int64','float16','float32','float64']
+    df_train=X_train.select_dtypes(include=numerics)
+    df_test=X_test.select_dtypes(include=numerics)
 
+    for i in df_train:
+        df_train[i]=np.sqrt(df_train[i])
+
+    for i in df_test:
+        df_test[i]=np.sqrt(df_test[i])
+    df_train_cat=X_train[['Dependents','Self_Employed','Property_Area']]
+    df_train_cat=pd.get_dummies(df_train_cat)
+    df_test_cat=X_test[['Dependents','Self_Employed','Property_Area']]
+    df_test_cat=pd.get_dummies(df_test_cat)
+    rx=pd.concat([df_train,df_train_cat],axis=1)
+    ry=pd.concat([df_test,df_test_cat],axis=1)
+
+    return rx,ry,y_train,y_test
+
+#data_cleaning_2 (X_train, X_test, y_train, y_test)
