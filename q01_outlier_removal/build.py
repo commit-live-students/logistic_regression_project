aaa463312a8864342c2ddb77fe1fca22bc7d233c
swapnilrt <swapnil13488@gmail.com>
2017-11-13 17:59:21 +0000

    Done

--- q01_outlier_removal/build.py
+++ q01_outlier_removal/build.py
@@ -1,4 +1,5 @@
 # Default imports
+
 import pandas as pd
 
 loan_data = pd.read_csv('data/loan_prediction_uncleaned.csv')
@@ -7,5 +8,12 @@ loan_data = loan_data.drop('Loan_ID', 1)
 
 # Write your Solution here:
 
-
-
+def outlier_removal(dataset):
+    a = dataset.quantile(q=0.95,interpolation = 'lower')
+    Q_ApplicantIncome = a['ApplicantIncome']
+    Q_CoapplicantIncome = a['CoapplicantIncome']
+    Q_LoanAmount = a['LoanAmount']
+    df1 = dataset.drop(dataset[(dataset['LoanAmount']>Q_LoanAmount)].index)
+    df2 = df1.drop(df1[(df1['ApplicantIncome']>Q_ApplicantIncome)].index)
+    df3 = df2.drop(df2[ (df2['CoapplicantIncome']>Q_CoapplicantIncome)].index)
+    return df3
