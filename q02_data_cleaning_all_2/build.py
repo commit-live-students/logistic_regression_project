6b062e94d9989c5b21428d074efd7ed61993171f
vp999 <patilvish@gmail.com>
2018-02-17 18:08:04 +0000

    Done

--- q02_data_cleaning_all_2/build.py
+++ q02_data_cleaning_all_2/build.py
@@ -17,13 +17,21 @@ def data_cleaning_2 (X_train, X_test, y_train, y_test):
         X_train.loc[:,col]=np.sqrt(X_train[col])
         X_test.loc[:,col]=np.sqrt(X_test[col])
 
-    cat_features=['Gender', 'Married', 'Dependents','Education', 'Self_Employed', 'Property_Area']
-    for col in cat_features:
-        X_train=pd.concat([X_train, pd.get_dummies(X_train[col],prefix =col)], axis=1);
-        X_test=pd.concat([X_test, pd.get_dummies(X_test[col],prefix =col)], axis=1);
-    X_train=X_train.drop(cat_features,axis=1)
-    X_test=X_test.drop(cat_features,axis=1)
+    #todo : find out why this was giving extra columns ?
+    #cat_features=['Gender', 'Married', 'Dependents','Education', 'Self_Employed', 'Property_Area']
+    #for col in cat_features:
+        #X_train=pd.concat([X_train, pd.get_dummies(X_train[col],prefix =col)], axis=1);
+        #X_test=pd.concat([X_test, pd.get_dummies(X_test[col],prefix =col)], axis=1);
+    #X_train=X_train.drop(cat_features,axis=1)
+    #X_test=X_test.drop(cat_features,axis=1)
+
+    cat_cols = ['Gender', 'Married', 'Dependents', 'Education', 'Self_Employed', 'Property_Area']
+    X_train = pd.get_dummies(X_train, columns=cat_cols, drop_first=True)
+    X_test = pd.get_dummies(X_test, columns=cat_cols, drop_first=True)
+
     return X_train, X_test, y_train, y_test
+
+
 
 X_train, X_test, y_train, y_test = data_cleaning_2 (X_train, X_test, y_train, y_test)
 print (X_train.shape)
