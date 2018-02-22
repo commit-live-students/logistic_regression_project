917acc760fe7793b3fcc857d8052f20353ce665c
ms10031990 <ms10031990@yahoo.com>
2018-02-22 06:52:13 +0000

    Done

--- q02_data_cleaning_all/build.py
+++ q02_data_cleaning_all/build.py
@@ -36,3 +36,31 @@ def data_cleaning(data):
     X_test['Credit_History'] = X_test['Credit_History'].fillna(X_test['Credit_History'].mode()[0])
 
     return X,y,X_train,X_test,y_train,y_test
+
+''' Optimized way
+def data_cleaning(data):
+    #find X and y
+    X,y = data.loc[:, data.columns != 'Loan_Status'], data[ 'Loan_Status']
+
+    #split train-test (25% test)
+    X_train,X_test,y_train,y_test= train_test_split(X,y, test_size=0.25, random_state=9)
+
+    num_features =['LoanAmount']
+    cat_features=['Gender', 'Married', 'Dependents', 'Self_Employed', 'Loan_Amount_Term', 'Credit_History']
+
+    #find mean of train for numeric features
+    for col in num_features:
+        mean_train= X_train[col].mean()
+        #impute missing data in train and test  with mean
+        X_train.loc[:,col]=X_train[col].fillna(mean_train)
+        X_test.loc[:,col]=X_test[col].fillna(mean_train)
+
+    for col in cat_features:
+        #find mode of train data for categorical features
+        mode_train= X_train[col].mode()[0]
+        #impute missing data in train and test  with mode
+        X_train.loc[:,col]=X_train[col].fillna(mode_train)
+        X_test.loc[:,col]=X_test[col].fillna(mode_train)
+
+    return X,y,X_train,X_test,y_train,y_test
+'''
--- q02_data_cleaning_all_2/build.py
+++ q02_data_cleaning_all_2/build.py
@@ -9,5 +9,15 @@ loan_data = loan_data.drop('Loan_ID', 1)
 loan_data = outlier_removal(loan_data)
 X, y, X_train, X_test, y_train, y_test = data_cleaning(loan_data)
 
+def data_cleaning_2(X_train, X_test, y_train, y_test):
+    num_features =['LoanAmount','ApplicantIncome', 'CoapplicantIncome']
 
-# Write your solution here :
+    for col in num_features:
+        X_train.loc[:,col]=np.sqrt(X_train[col])
+        X_test.loc[:,col]=np.sqrt(X_test[col])
+
+    cat_cols = ['Gender', 'Married', 'Dependents', 'Education', 'Self_Employed', 'Property_Area']
+    X_train = pd.get_dummies(X_train, columns=cat_cols, drop_first=True)
+    X_test = pd.get_dummies(X_test, columns=cat_cols, drop_first=True)
+
+    return X_train, X_test, y_train, y_test
