5f91f069bdeee5c58dba5da46b764863a821fe29
cchopade <cchopade@gmail.com>
2018-02-26 09:44:13 +0000

    Done

--- q02_data_cleaning_all_2/build.py
+++ q02_data_cleaning_all_2/build.py
@@ -9,5 +9,18 @@ loan_data = loan_data.drop('Loan_ID', 1)
 loan_data = outlier_removal(loan_data)
 X, y, X_train, X_test, y_train, y_test = data_cleaning(loan_data)
 
+def data_cleaning_2(X_train, X_test, y_train, y_test):
+
+    cat = ['Gender', 'Married', 'Dependents', 'Education', 'Self_Employed', 'Property_Area']
+    all_col = X_train.columns.values
+
+    num = [x for x in all_col if x not in cat]
+    print num
+    X_train[num] = np.sqrt(X_train[num])
+
+    X_etrain = pd.get_dummies(X_train,columns = cat,drop_first = True)
+    X_etest = pd.get_dummies(X_test, columns = cat, drop_first = True)
+
+    return X_etrain, X_etest, y_train, y_test
 
 # Write your solution here :
