4520e065a8cde6227e1b418d178686ccdaac88d4
pranaliangre <pranali.r.c@gmail.com>
2018-03-14 13:55:16 +0000

    Done

--- q03_logistic_regression/build.py
+++ q03_logistic_regression/build.py
@@ -1,11 +1,12 @@
+
 # Default Imports
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix
-from logistic_regression_project.q01_outlier_removal.build import outlier_removal
-from logistic_regression_project.q02_data_cleaning_all.build import data_cleaning
-from logistic_regression_project.q02_data_cleaning_all_2.build import data_cleaning_2
+from greyatomlib.logistic_regression_project.q01_outlier_removal.build import outlier_removal
+from greyatomlib.logistic_regression_project.q02_data_cleaning_all.build import data_cleaning
+from greyatomlib.logistic_regression_project.q02_data_cleaning_all_2.build import data_cleaning_2
 
 loan_data = pd.read_csv('data/loan_prediction_uncleaned.csv')
 loan_data = loan_data.drop('Loan_ID', 1)
@@ -15,4 +16,26 @@ X_train, X_test, y_train, y_test = data_cleaning_2(X_train, X_test, y_train, y_t
 
 
 # Write your solution code here:
+def logistic_regression(X_train, X_test, y_train, y_test):
+    # Feature Scaling
+    sc = StandardScaler()
+    X_train[['ApplicantIncome', 'CoapplicantIncome', 'LoanAmount']] = sc.fit_transform(
+        X_train[['ApplicantIncome', 'CoapplicantIncome', 'LoanAmount']])
+    X_test[['ApplicantIncome', 'CoapplicantIncome', 'LoanAmount']] = sc.transform(
+        X_test[['ApplicantIncome', 'CoapplicantIncome', 'LoanAmount']])
+
+    # Fitting Logistic Regression to the Training set
+
+    classifier = LogisticRegression(random_state=9)
+    classifier.fit(X_train, y_train)
+
+    # Predicting the Test set results
+    y_pred = classifier.predict(X_test)
+
+    # Making the Confusion Matrix
+
+    cm = confusion_matrix(y_test, y_pred)
+    return cm
+
+
 
