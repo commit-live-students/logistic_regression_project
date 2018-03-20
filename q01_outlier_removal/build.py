03039b60916047e9ad02b229bf68cd45d05d293d
PBPatil <theprasadpatil@gmail.com>
2018-03-20 04:20:07 +0000

    Done

--- q01_outlier_removal/build.py
+++ q01_outlier_removal/build.py
@@ -6,3 +6,11 @@ loan_data = loan_data.drop('Loan_ID', 1)
 
 
 # Write your Solution here:
+def outlier_removal(dataset):
+    qual_vals = dataset.quantile(0.95)
+
+    dataset =dataset.drop(dataset[dataset['ApplicantIncome'] > qual_vals[0]].index)
+    dataset= dataset.drop(dataset[dataset['CoapplicantIncome'] > qual_vals[1]].index)
+    dataset= dataset.drop(dataset[dataset['LoanAmount'] > qual_vals[2]].index)
+
+    return dataset
