7b88ccbb1a2c0200cb0428baa516d9cb095fd795
AdityaMV1215 <aditya.vadhavkar12@gmail.com>
2018-04-02 11:42:24 +0000

    Done

--- q02_data_cleaning_all/build.py
+++ q02_data_cleaning_all/build.py
@@ -1,3 +1,4 @@
+# %load q02_data_cleaning_all/build.py
 # Default Imports
 import sys, os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
@@ -12,3 +13,21 @@ loan_data = outlier_removal(loan_data)
 
 
 # Write your solution here :
+def data_cleaning(data):
+    np.random.seed(9)
+    X = data.iloc[:,:-1]
+    y = data.iloc[:,-1]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=9)
+    X_train_loanamt = X_train[X_train['LoanAmount'].notnull()]['LoanAmount'].mean()
+    X_train.loc[X_train['LoanAmount'].isnull(), 'LoanAmount'] = X_train_loanamt
+    X_test.loc[X_test['LoanAmount'].isnull(), 'LoanAmount'] = X_train_loanamt
+    cat = ['Gender', 'Married', 'Dependents', 'Self_Employed', 'Loan_Amount_Term', 'Credit_History']
+    for val in cat:
+        temp = X_train[X_train[val].notnull()][val].mode()
+        X_train.loc[X_train[val].isnull(), val] = temp
+        X_test.loc[X_test[val].isnull(), val] = temp
+
+    return X, y, X_train, X_test, y_train, y_test
+
+
+
