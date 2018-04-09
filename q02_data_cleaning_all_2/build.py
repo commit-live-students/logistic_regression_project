d8c2627bb414cedcdbe30e41b4f16be0fffc6833
sonal7930 <sonal.chandra7930@gmail.com>
2018-04-09 11:27:35 +0000

    Done

--- q02_data_cleaning_all_2/build.py
+++ q02_data_cleaning_all_2/build.py
@@ -1,13 +1,21 @@
 # Default Imports
 import pandas as pd
 import numpy as np
-from logistic_regression_project.q02_data_cleaning_all.build import data_cleaning
-from logistic_regression_project.q01_outlier_removal.build import outlier_removal
+from greyatomlib.logistic_regression_project.q02_data_cleaning_all.build import data_cleaning
+from greyatomlib.logistic_regression_project.q01_outlier_removal.build import outlier_removal
+from scipy.stats import skew
+from sklearn.preprocessing import LabelEncoder
 
 loan_data = pd.read_csv('data/loan_prediction_uncleaned.csv')
 loan_data = loan_data.drop('Loan_ID', 1)
 loan_data = outlier_removal(loan_data)
 X, y, X_train, X_test, y_train, y_test = data_cleaning(loan_data)
 
-
-# Write your solution here :
+def data_cleaning_2(X_train,X_test,y_train,y_test):
+    X_train["ApplicantIncome"] = np.sqrt(X_train['ApplicantIncome'])
+    X_test["ApplicantIncome"]=np.sqrt(X_test["ApplicantIncome"])
+    X_train= pd.get_dummies(X_train, columns=["Gender","Married","Dependents","Education","Self_Employed","Property_Area"])
+    X_test= pd.get_dummies(X_test, columns=["Gender","Married","Dependents","Education","Self_Employed","Property_Area"])
+    X_train=X_train.drop(['Dependents_0','Gender_Female','Education_Graduate','Self_Employed_No','Married_No','Property_Area_Rural'],axis=1)
+    X_test=X_test.drop(['Dependents_0','Gender_Female','Education_Graduate','Self_Employed_No','Married_No','Property_Area_Rural'],axis=1)
+    return X_train,X_test,y_train,y_test
--- q03_logistic_regression/build.py
+++ q03_logistic_regression/build.py
@@ -3,9 +3,9 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix
-from logistic_regression_project.q01_outlier_removal.build import outlier_removal
-from logistic_regression_project.q02_data_cleaning_all.build import data_cleaning
-from logistic_regression_project.q02_data_cleaning_all_2.build import data_cleaning_2
+from greyatomlib.logistic_regression_project.q01_outlier_removal.build import outlier_removal
+from greyatomlib.logistic_regression_project.q02_data_cleaning_all.build import data_cleaning
+from greyatomlib.logistic_regression_project.q02_data_cleaning_all_2.build import data_cleaning_2
 
 loan_data = pd.read_csv('data/loan_prediction_uncleaned.csv')
 loan_data = loan_data.drop('Loan_ID', 1)
@@ -13,6 +13,5 @@ loan_data = outlier_removal(loan_data)
 X, y, X_train, X_test, y_train, y_test = data_cleaning(loan_data)
 X_train, X_test, y_train, y_test = data_cleaning_2(X_train, X_test, y_train, y_test)
 
-
+print(X_train.info())
 # Write your solution code here:
-
