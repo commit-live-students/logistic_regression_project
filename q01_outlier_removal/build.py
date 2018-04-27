cc44f6df8f32db127bdc06f183b0f6439d36d3f6
santoshmayekar <santosh.mayekar@outlook.com>
2018-04-27 13:01:24 +0000

    Done

--- q01_outlier_removal/build.py
+++ q01_outlier_removal/build.py
@@ -1,8 +1,14 @@
-# Default imports
 import pandas as pd
+import matplotlib.pyplot as plt
 
 loan_data = pd.read_csv('data/loan_prediction_uncleaned.csv')
 loan_data = loan_data.drop('Loan_ID', 1)
 
+def outlier_removal(data):
+    series1 = data.quantile(0.95)
+    for i in range(0,len(series1)-2):
+        data = data.drop(data[(data[series1.index[i]] > series1[i])].index)
+    return data
 
-# Write your Solution here:
+
+
