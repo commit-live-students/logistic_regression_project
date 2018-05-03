130b29cc12535f9e6f425c66ac1eb3e5df62d0a7
kvin15 <vinayakkhamkar@gmail.com>
2018-05-03 17:30:54 +0000

    Done

--- q01_outlier_removal/build.py
+++ q01_outlier_removal/build.py
@@ -6,3 +6,13 @@ loan_data = loan_data.drop('Loan_ID', 1)
 
 
 # Write your Solution here:
+def outlier_removal(data):
+    quantile_threshold = 0.95
+    threshold_appIncome = data.ApplicantIncome.quantile(quantile_threshold)
+    threshold_coAppIncome = data.CoapplicantIncome.quantile(quantile_threshold)
+    threshold_LoanAmount  = data.LoanAmount.quantile(quantile_threshold)
+    outlier_criteria_index = data[(data.ApplicantIncome > threshold_appIncome)\
+                             | (data.CoapplicantIncome > threshold_coAppIncome)\
+                             | (data.LoanAmount > threshold_LoanAmount)].index
+    data.drop(labels=outlier_criteria_index, axis=0, inplace=True)
+    return data
