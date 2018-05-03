23f644db17df2cbd3a9adad4e32f05d2667b1700
kvin15 <vinayakkhamkar@gmail.com>
2018-05-03 18:12:50 +0000

    Done

--- q02_data_cleaning_all/build.py
+++ q02_data_cleaning_all/build.py
@@ -12,3 +12,16 @@ loan_data = outlier_removal(loan_data)
 
 
 # Write your solution here :
+def data_cleaning(data):
+    random_seed = 9
+    test_size = 0.25
+    mean_loan_amt = data['LoanAmount'].mean()
+    data['LoanAmount'].fillna(value=mean_loan_amt, inplace=True)
+    category_columns = ['Gender', 'Married', 'Dependents', 'Self_Employed', 'Loan_Amount_Term', 'Credit_History']
+    for column in category_columns:
+        col_mode = data[column].mode()
+        data[column].fillna(value=col_mode[0], inplace=True)
+    X = data.drop(labels=['Loan_Status'],axis=1)
+    y = data.Loan_Status
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_seed)
+    return X, y, X_train, X_test, y_train, y_test
