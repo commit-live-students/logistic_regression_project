6e20cd488566f1d5315cbe39d548656f854c0c04
kvin15 <vinayakkhamkar@gmail.com>
2018-05-04 09:17:29 +0000

    Done

--- q02_data_cleaning_all/build.py
+++ q02_data_cleaning_all/build.py
@@ -1,6 +1,6 @@
 # Default Imports
 import sys, os
-sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
+# sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
