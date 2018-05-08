a463579a7af73b6398b0c7a2f8736a7b0e85ad8c
santoshmayekar <santosh.mayekar@outlook.com>
2018-05-08 12:24:24 +0000

    Done

--- q03_logistic_regression/build.py
+++ q03_logistic_regression/build.py
@@ -1,11 +1,11 @@
-# Default Imports
 import pandas as pd
+import numpy as np
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix
-from logistic_regression_project.q01_outlier_removal.build import outlier_removal
-from logistic_regression_project.q02_data_cleaning_all.build import data_cleaning
-from logistic_regression_project.q02_data_cleaning_all_2.build import data_cleaning_2
+from greyatomlib.logistic_regression_project.q01_outlier_removal.build import outlier_removal
+from greyatomlib.logistic_regression_project.q02_data_cleaning_all.build import data_cleaning
+from greyatomlib.logistic_regression_project.q02_data_cleaning_all_2.build import data_cleaning_2
 
 loan_data = pd.read_csv('data/loan_prediction_uncleaned.csv')
 loan_data = loan_data.drop('Loan_ID', 1)
@@ -13,6 +13,14 @@ loan_data = outlier_removal(loan_data)
 X, y, X_train, X_test, y_train, y_test = data_cleaning(loan_data)
 X_train, X_test, y_train, y_test = data_cleaning_2(X_train, X_test, y_train, y_test)
 
+def logistic_regression(X_train,X_test,y_train,y_test):
+    scaler=StandardScaler()
+    scaler.fit_transform(X_train)
+    scaler.fit_transform(X_test)
+    logreg = LogisticRegression()
+    logreg.fit(X_train, y_train)
+    y_pred = logreg.predict(X_test)
+    cm = confusion_matrix(y_test, y_pred)
+    return cm
 
-# Write your solution code here:
 
