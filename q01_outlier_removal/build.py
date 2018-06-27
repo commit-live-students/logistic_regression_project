eb515aaaa85a46f8cd5be4793de23bdd603aacaf
HardikaB <hardi.2607@gmail.com>
2018-06-27 11:54:03 +0000

    Done

--- q01_outlier_removal/build.py
+++ q01_outlier_removal/build.py
@@ -1,3 +1,4 @@
+# %load q01_outlier_removal/build.py
 # Default imports
 import pandas as pd
 
@@ -6,3 +7,13 @@ loan_data = loan_data.drop('Loan_ID', 1)
 
 
 # Write your Solution here:
+def outlier_removal(loan_data):
+    numeric_columns=['ApplicantIncome', 'CoapplicantIncome', 'LoanAmount']
+    IQRU=list()
+    for c in numeric_columns:
+        IQRU.append([c,loan_data[c].quantile(0.95)])
+    for c in IQRU:
+        loan_data=loan_data.drop(loan_data[(loan_data[c[0]]>c[1])].index)
+    return loan_data
+
+
