421bf2b55fcf3995d15d5621beaf74c520f5ef24
asdoc <asdocode@gmail.com>
2018-06-30 18:44:50 +0000

    Done

--- q03_logistic_regression/build.py
+++ q03_logistic_regression/build.py
@@ -1,11 +1,12 @@
+# %load q03_logistic_regression/build.py
 # Default Imports
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix
-from logistic_regression_project.q01_outlier_removal.build import outlier_removal
-from logistic_regression_project.q02_data_cleaning_all.build import data_cleaning
-from logistic_regression_project.q02_data_cleaning_all_2.build import data_cleaning_2
+from greyatomlib.logistic_regression_project.q01_outlier_removal.build import outlier_removal
+from greyatomlib.logistic_regression_project.q02_data_cleaning_all.build import data_cleaning
+from greyatomlib.logistic_regression_project.q02_data_cleaning_all_2.build import data_cleaning_2
 
 loan_data = pd.read_csv('data/loan_prediction_uncleaned.csv')
 loan_data = loan_data.drop('Loan_ID', 1)
@@ -14,5 +15,10 @@ X, y, X_train, X_test, y_train, y_test = data_cleaning(loan_data)
 X_train, X_test, y_train, y_test = data_cleaning_2(X_train, X_test, y_train, y_test)
 
 
-# Write your solution code here:
+def logistic_regression(X_train, X_test, y_train, y_test):
+    model = LogisticRegression(random_state=9)
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    return confusion_matrix(y_test, y_pred)
+
 
