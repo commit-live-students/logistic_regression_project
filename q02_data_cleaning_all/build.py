c75989649b4a42818c7fde1e2ff45f676e7ec478
bhushan45 <bhushan45@users.noreply.github.com>
2018-07-18 16:09:52 +0530

    Update build.py

--- q02_data_cleaning_all/build.py
+++ q02_data_cleaning_all/build.py
@@ -1,10 +1,10 @@
 # Default Imports
 import sys, os
-sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname('__file__'))))
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
-from logistic_regression_project.q01_outlier_removal.build import outlier_removal
+from greyatomlib.logistic_regression_project.q01_outlier_removal.build import outlier_removal
 
 loan_data = pd.read_csv('data/loan_prediction_uncleaned.csv')
 loan_data = loan_data.drop('Loan_ID', 1)
