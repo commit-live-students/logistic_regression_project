a350b0309cc39a2231d7cefc02aee463ce7ae53a
bhushan45 <bhushan45@users.noreply.github.com>
2018-07-18 16:11:24 +0530

    Update build.py

--- q02_data_cleaning_all_2/build.py
+++ q02_data_cleaning_all_2/build.py
@@ -1,8 +1,8 @@
 # Default Imports
 import pandas as pd
 import numpy as np
-from logistic_regression_project.q02_data_cleaning_all.build import data_cleaning
-from logistic_regression_project.q01_outlier_removal.build import outlier_removal
+from greyatomlib.logistic_regression_project.q02_data_cleaning_all.build import data_cleaning
+from greyatomlib.logistic_regression_project.q01_outlier_removal.build import outlier_removal
 
 loan_data = pd.read_csv('data/loan_prediction_uncleaned.csv')
 loan_data = loan_data.drop('Loan_ID', 1)
