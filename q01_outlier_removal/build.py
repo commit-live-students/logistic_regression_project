efb3e85d4fa9e36735daf581866c94226a7d0640
SL345 <sherilyn345@gmail.com>
2018-07-23 05:57:40 +0000

    Done

--- q01_outlier_removal/build.py
+++ q01_outlier_removal/build.py
@@ -1,3 +1,4 @@
+# %load q01_outlier_removal/build.py
 # Default imports
 import pandas as pd
 
@@ -6,3 +7,7 @@ loan_data = loan_data.drop('Loan_ID', 1)
 
 
 # Write your Solution here:
+def outlier_removal(loan_data):
+    loan_data = loan_data[(loan_data.LoanAmount <= loan_data.LoanAmount.quantile(0.962)) & (loan_data.LoanAmount >= loan_data.LoanAmount.quantile(0.04))]
+    return loan_data
+
