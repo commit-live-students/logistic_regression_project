7fd9627aba9bed14cd5b299dcafc3553db177512
siddesh001 <siddesh.001@gmail.com>
2018-08-28 15:27:12 +0000

    Done

--- q03_logistic_regression/build.py
+++ q03_logistic_regression/build.py
@@ -1,3 +1,4 @@
+# %load q03_logistic_regression/build.py
 # Default Imports
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
@@ -10,9 +11,25 @@ from greyatomlib.logistic_regression_project.q02_data_cleaning_all_2.build impor
 loan_data = pd.read_csv('data/loan_prediction_uncleaned.csv')
 loan_data = loan_data.drop('Loan_ID', 1)
 loan_data = outlier_removal(loan_data)
+
+
+
 X, y, X_train, X_test, y_train, y_test = data_cleaning(loan_data)
 X_train, X_test, y_train, y_test = data_cleaning_2(X_train, X_test, y_train, y_test)
 
-
 # Write your solution code here:
+
+def logistic_regression(X_train, X_test, y_train, y_test):
+    scalar=StandardScaler()
+    sc=['ApplicantIncome', 'CoapplicantIncome', 'LoanAmount']
+    X_train[sc]=scalar.fit_transform(X_train[sc])
+    X_test[sc]=scalar.fit_transform(X_test[sc]) 
+    lr=LogisticRegression()
+    lr.fit(X_train,y_train)
+    predict=lr.predict(X_test)
+    return confusion_matrix(y_test,predict)
+    
+    
+    
+
 
