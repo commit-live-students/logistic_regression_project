256e3f3bbd1f7d11c398a3e4776b11ca5e29517e
sonik8494 <sourabh.niks1994@gmail.com>
2018-10-03 16:36:04 +0000

    Done

--- q02_data_cleaning_all/build.py
+++ q02_data_cleaning_all/build.py
@@ -1,3 +1,4 @@
+# %load q02_data_cleaning_all/build.py
 # Default Imports
 import sys, os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname('__file__'))))
@@ -12,3 +13,19 @@ loan_data = outlier_removal(loan_data)
 
 
 # Write your solution here :
+def data_cleaning(loan_data):
+    
+    np.random.seed(9)
+    for x in loan_data.columns:
+        if loan_data[x].dtypes == np.number:
+            loan_data[x] = loan_data[x].fillna(loan_data[x].mean())
+        if loan_data[x].dtypes == 'object':
+            loan_data[x] = loan_data[x].fillna(loan_data[x].mode()[0])
+    X = loan_data.drop('Loan_Status',axis = 1)
+    y = loan_data['Loan_Status']
+    X_train,X_test,y_train,y_test = train_test_split(X,y,test_size = 0.25)
+    return X,y,X_train,X_test,y_train,y_test
+data_cleaning(loan_data)
+
+
+
