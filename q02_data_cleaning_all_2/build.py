81fa7ea6cf3796e42e322aedb22ed3cb94d98eae
sonik8494 <sourabh.niks1994@gmail.com>
2018-10-05 07:21:45 +0000

    Done

--- q02_data_cleaning_all_2/build.py
+++ q02_data_cleaning_all_2/build.py
@@ -1,3 +1,4 @@
+# %load q02_data_cleaning_all_2/build.py
 # Default Imports
 import pandas as pd
 import numpy as np
@@ -11,3 +12,18 @@ X, y, X_train, X_test, y_train, y_test = data_cleaning(loan_data)
 
 
 # Write your solution here :
+def data_cleaning_2(X_train,X_test,y_train,y_test):
+    
+    for x in ['CoapplicantIncome','LoanAmount','Loan_Amount_Term','Credit_History']:
+    
+        X_train[x] = np.sqrt(X_train[x])  
+        X_test[x]= np.sqrt(X_test[x])
+    
+    X_train1 = pd.get_dummies(X_train)
+    X_test1 = pd.get_dummies(X_test)
+    X_train1=X_train1.drop(['Dependents_0','Gender_Female','Education_Graduate','Self_Employed_No','Married_No','Property_Area_Rural'],axis=1)
+    X_test1=X_test1.drop(['Dependents_0','Gender_Female','Education_Graduate','Self_Employed_No','Married_No','Property_Area_Rural'],axis=1)
+    return X_train1,X_test1,y_train,y_test
+X_test.shape
+
+
