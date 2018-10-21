4ec9b23a3d4734b0878be8058beb3e78c6458cae
Tusharsharma86 <tusharsharma86@gmail.com>
2018-10-21 04:47:13 +0000

    Done

--- q01_outlier_removal/build.py
+++ q01_outlier_removal/build.py
@@ -1,8 +1,28 @@
+# %load q01_outlier_removal/build.py
 # Default imports
 import pandas as pd
 
 loan_data = pd.read_csv('data/loan_prediction_uncleaned.csv')
 loan_data = loan_data.drop('Loan_ID', 1)
 
+# Function will remove the outliers
+def outlier_removal(loan_data):
+    a = loan_data['ApplicantIncome'].quantile(0.95) # It comes out to be 14583.0
+    b = loan_data['CoapplicantIncome'].quantile(0.95) # It comes out to be 4997.4
+    c = loan_data['LoanAmount'].quantile(0.95) # It comes out to be 297.8
+    loan_data = loan_data.drop(loan_data[loan_data['ApplicantIncome'] > a].index)
+    loan_data = loan_data.drop(loan_data[loan_data['CoapplicantIncome'] > b].index)
+    loan_data = loan_data.drop(loan_data[loan_data['LoanAmount'] > c].index)
+    return loan_data
 
-# Write your Solution here:
+outlier_removal(loan_data).shape
+
+
+
+
+
+
+
+
+
+
--- q02_data_cleaning_all/build.py
+++ q02_data_cleaning_all/build.py
@@ -1,3 +1,4 @@
+# %load q02_data_cleaning_all/build.py
 # Default Imports
 import sys, os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname('__file__'))))
@@ -12,3 +13,23 @@ loan_data = outlier_removal(loan_data)
 
 
 # Write your solution here :
+def data_cleaning(loan_data):
+    
+    loan_data['LoanAmount'].fillna(loan_data['LoanAmount'].mean(), inplace = True)
+    
+    cat_col = ['Gender', 'Married', 'Dependents', 'Self_Employed', 'Loan_Amount_Term', 'Credit_History']
+    for col in cat_col:
+        loan_data['LoanAmount'].fillna(loan_data[col].mode(), inplace = True)
+    
+    X = loan_data.iloc[:,:-1]
+    y = loan_data.iloc[:,-1:]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 9)
+    return X, y, X_train, X_test, y_train, y_test
+
+data_cleaning(loan_data)
+loan_data
+
+
+
+
+
