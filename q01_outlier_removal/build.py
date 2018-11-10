b538bbf80d51cbef8c5747c884983ec475cb2e13
Tusharshah2006 <tusharshah2006@gmail.com>
2018-11-10 03:09:23 +0000

    Done

--- q01_outlier_removal/build.py
+++ q01_outlier_removal/build.py
@@ -1,4 +1,6 @@
+# %load q01_outlier_removal/build.py
 # Default imports
+import numpy as np
 import pandas as pd
 
 loan_data = pd.read_csv('data/loan_prediction_uncleaned.csv')
@@ -6,3 +8,12 @@ loan_data = loan_data.drop('Loan_ID', 1)
 
 
 # Write your Solution here:
+def outlier_removal(data):
+    data = data[np.invert((data['ApplicantIncome'] > data['ApplicantIncome'].quantile(0.95)) | 
+                (data['CoapplicantIncome'] > data['CoapplicantIncome'].quantile(0.95)) | 
+                (data['LoanAmount'] > data['LoanAmount'].quantile(0.95)))]
+    return data    
+    
+outlier_removal(loan_data)
+
+
