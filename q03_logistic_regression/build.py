63aab1882942a8853d84fda79a37165b3815fe9e
preetiail <ailpreeti@ymail.com>
2019-01-18 16:44:25 +0000

    Done

--- q03_logistic_regression/build.py
+++ q03_logistic_regression/build.py
@@ -1,3 +1,4 @@
+# %load q03_logistic_regression/build.py
 # Default Imports
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
@@ -12,7 +13,20 @@ loan_data = loan_data.drop('Loan_ID', 1)
 loan_data = outlier_removal(loan_data)
 X, y, X_train, X_test, y_train, y_test = data_cleaning(loan_data)
 X_train, X_test, y_train, y_test = data_cleaning_2(X_train, X_test, y_train, y_test)
+def logistic_regression(X_train, X_test, y_train, y_test):
+    #Scaling the data
+    scaler = StandardScaler()
+    StandardScaler(copy=True, with_mean=True, with_std=True)
+    scaler.fit(X_train)
+    scaler.transform(X_train)
+    scaler.fit(X_test)
+    scaler.transform(X_test)
+    #Logistic regression
+    model1=LogisticRegression(random_state=9).fit(X_train,y_train)
+    ypred=model1.predict(X_test)
+    #Confusion matrix
+    result=confusion_matrix(y_test, ypred)
+    return(result)
 
-
-# Write your solution code here:
+logistic_regression(X_train, X_test, y_train, y_test)
 
