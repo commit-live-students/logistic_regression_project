307caf38738bf50e3b9cd55dd64f02ebfe3e325b
tracedence <shoaibrashid887@gmail.com>
2019-01-24 14:24:07 +0000

    Done

--- q01_outlier_removal/build.py
+++ q01_outlier_removal/build.py
@@ -1,3 +1,4 @@
+# %load q01_outlier_removal/build.py
 # Default imports
 import pandas as pd
 
@@ -6,3 +7,19 @@ loan_data = loan_data.drop('Loan_ID', 1)
 
 
 # Write your Solution here:
+def outlier_removal(data):
+    
+    loan_data = data
+    loan_data = loan_data[loan_data['ApplicantIncome'] < loan_data['ApplicantIncome'].quantile(0.97)]
+    loan_data = loan_data[loan_data['CoapplicantIncome'] < loan_data['CoapplicantIncome'].quantile(0.98)]
+    loan_data = loan_data[loan_data['LoanAmount'] < loan_data['LoanAmount'].quantile(0.97)]
+    
+    return loan_data
+# loan_data = loan_data[loan_data['ApplicantIncome'] < loan_data['ApplicantIncome'].quantile(0.97)]
+# loan_data = loan_data[loan_data['CoapplicantIncome'] < loan_data['CoapplicantIncome'].quantile(0.98)]
+# loan_data = loan_data[loan_data['LoanAmount'] < loan_data['LoanAmount'].quantile(0.97)]
+# loan_data = loan_data[(loan_data['ApplicantIncome'] < loan_data['ApplicantIncome'].quantile(0.95)) & (loan_data['CoapplicantIncome'] < loan_data['CoapplicantIncome'].quantile(0.95)) & (loan_data['LoanAmount'] < loan_data['LoanAmount'].quantile(0.95))]
+# loan_data.shape
+
+
+
