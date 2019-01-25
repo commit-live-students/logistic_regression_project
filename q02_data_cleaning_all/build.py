c4f1e5195c30fc56effaaaead3704c408780dcc7
tracedence <shoaibrashid887@gmail.com>
2019-01-25 14:08:57 +0000

    Done

--- q02_data_cleaning_all/build.py
+++ q02_data_cleaning_all/build.py
@@ -1,3 +1,4 @@
+# %load q02_data_cleaning_all/build.py
 # Default Imports
 import sys, os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname('__file__'))))
@@ -12,3 +13,19 @@ loan_data = outlier_removal(loan_data)
 
 
 # Write your solution here :
+
+def data_cleaning(data):
+    
+    data['LoanAmount'].fillna(data['LoanAmount'].mean(),inplace = True)
+    cat_list = ['Gender','Married', 'Dependents', 'Self_Employed','Loan_Amount_Term','Credit_History']
+    for e in cat_list:
+        data[e].fillna(data[e].mode()[0],inplace = True)
+    X = data.iloc[:,:-1]
+    y = data.iloc[:,-1]
+    X_train, X_test, y_train, y_test = train_test_split(X,y, test_size = 0.25, random_state= 9)
+    
+    return X,y, X_train, X_test, y_train, y_test
+
+data_cleaning(loan_data)
+
+
